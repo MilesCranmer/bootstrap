c04bdcdb27ed78566ddbf1a909cbe1dcddc83306
MilesCranmer <miles.cranmer@gmail.com>
2017-05-30 11:10:28 +1000

    Add part to install ncurses

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-wget https://downloads.sourceforge.net/project/zsh/zsh/5.3.1/zsh-5.3.1.tar.xz
-tar xzf zsh-5.3.1.tar.xz
-rm zsh-5.3.1.tar.xz
-cd zsh-5.3.1
-./configure
+wget -O ncurses.tar.gz https://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz
+mkdir ncurses
+tar -xvzf ncurses.tar.gz -C ncurses --strip-components 1
+rm ncurses.tar.gz
+cd ncurses
+export CXXFLAGS="$CXXFLAGS -fPIC"
+export CFLAGS="$CFLAGS -fPIC"
+./configure --prefix=$HOME
 make
 make install
+cd ..
+rm -rf ncurses
+wget -O zsh.tar.gz https://sourceforge.net/projects/zsh/files/latest/download
+mkdir zsh
+tar -xvzf zsh.tar.gz -C zsh --strip-components 1
+rm zsh.tar.gz
+cd zsh
+export CPPFLAGS="$CPPFLAGS -I$HOME/include"
+export LDFLAGS="$LDFLAGS -L$HOME/lib"
+./configure --prefix=$HOME
+make
+make install
+cd ..
+rm -rf zsh
