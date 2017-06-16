779410494501a4018b70a4442bbc7ea95bc8bf58
Miles Cranmer <miles.cranmer@gmail.com>
2017-06-16 09:07:03 +0000

    Fix aws bootstrap

--- aws_setup.sh
+++ aws_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -ev
 
 sudo service docker start
@@ -13,11 +14,7 @@ sudo service docker start
 } || {
     echo "Already in docker group" > /dev/stdout
 }
-{
-    docker ps
-} || {
-    exit
-}
+
 # Install nvidia-docker and nvidia-docker-plugin
 wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1_amd64.tar.xz
 sudo tar --strip-components=1 -C /usr/bin -xvf /tmp/nvidia-docker*.tar.xz && rm /tmp/nvidia-docker*.tar.xz
